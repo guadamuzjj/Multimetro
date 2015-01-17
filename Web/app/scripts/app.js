ee0411c6c9a894ce17d36686570954f7b9adb343
oscarmcm <om.cortez.2010@gmail.com>
2015-01-17 17:36:59 -0600

    using AngularJS Routes

--- Web/app/scripts/app.js
+++ Web/app/scripts/app.js
@@ -1,6 +1,36 @@
-/*global define */
-define([], function () {
-    'use strict';
+// create the module and name it multimetro
+var multimetro = angular.module('multimetro', ['ngRoute']);
 
-    return '\'Allo \'Allo!';
+// configure our routes
+multimetro.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'mainController'
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+});
+// create the controller and inject Angular's $scope
+multimetro.controller('mainController', function($scope) {
+    // create a title to display in our view
+    $scope.appName = 'Multimetro';
+});
+
+multimetro.controller('aboutController', function($scope) {
+});
+
+multimetro.controller('contactController', function($scope) {
 });
