749825204f05bf7b6c2563a3224bffe863153a5d
oscarmcm <om.cortez.2010@gmail.com>
2015-01-20 23:20:33 -0600

    improved search

--- Web/app/scripts/app.js
+++ Web/app/scripts/app.js
@@ -1,5 +1,5 @@
 // create the module and name it multimetro
-var multimetro = angular.module('multimetro', ['ngRoute', 'ngProgress']);
+var multimetro = angular.module('multimetro', ['ngRoute', 'ngProgress', 'lbServices']);
 
 // configure our routes
 multimetro.config(function($routeProvider) {
@@ -24,19 +24,31 @@ multimetro.config(function($routeProvider) {
         });
 });
 // create the controller and inject Angular's $scope
-multimetro.controller('mainController', function($scope, ngProgress) {
+multimetro.controller('mainController', ['$scope', 'ngProgresste', 'Multas', function($scope, ngProgress, Multas) {
     // create a title to display in our view
     $scope.appName = 'Multimetro';
+    $scope.multas = [];
+
+    function getMultas() {
+      Todo
+        .find()
+        .$promise
+        .then(function(results) {
+          $scope.multas = results;
+        });
+    }
+    getTodos();
+
     ngProgress.start(2);
-    ngProgress.complete()
-});
+    ngProgress.complete();
+}]);
 
 multimetro.controller('aboutController', function($scope, ngProgress) {
     ngProgress.start(2);
-    ngProgress.complete()
+    ngProgress.complete();
 });
 
 multimetro.controller('contactController', function($scope, ngProgress) {
     ngProgress.start(2);
-    ngProgress.complete()
+    ngProgress.complete();
 });
